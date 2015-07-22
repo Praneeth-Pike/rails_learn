c8b4a1f28a493cd8f396cd2c0fe65a47d6cd9b9d
praneeth <praneeth-pike>
2015-07-22 10:23:31 +0000

    Replicated migrations and tables to match the course

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717121939) do
+ActiveRecord::Schema.define(version: 20150722101423) do
 
   create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
